7ff808d4271b12067f91cc53aadd9f81ede9e2f4
marcos1305 <marcos1305pt@gmail.com>
2018-06-14 00:08:24 -0300

    Pagina de balance do usuario

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::group(['middleware' => ['auth'], 'namespace' => 'Admin', 'prefix' => 'admin'], function(){
+    Route::get('balance', 'BalanceController@index')->name('admin.balance');
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::get('/', 'AdminController@index')->name('admin.home');
 });
+
+Route::get('/', 'Site\\SiteController@index')->name('home');
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
